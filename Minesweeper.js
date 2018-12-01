adc6dc645fb80edb8242759e8dfab23f3475a722
ninakim <ninakim97@gmail.com>
2018-11-30 20:56:33 -0500

    Update Minesweeper.js

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,19 +1,18 @@
 function checkMap(x, y){
-        return board[x+y*cols];
+        return cellValues[x+y*cols];
 }
 
-function picture(index)        // This function returns the name of the image of the tile (uncovered/flag/question mark).
+function picture(index){        // This function returns the name of the image of the tile (uncovered/flag/question mark).
                         // To be more precise, it returns the last but four letter of the filename of the image.
                         // It would be more elegant if we created a separate array to indicate it, but I chose this clunky way to shorten the code a bit.
-        {
-        return tile[index].src.substr(tile[index].src.length-5,1);
+        return cells[index].src.substr(cells[index].src.length-5,1);
         }
 
 function startGame(){
-        cols = document.getElementById("col").value;
-        rows = document.getElementById("row").value;
-        mines = document.getElementById("mine").value;
-        numCells = cols * rows;
+        var cols = document.getElementById("col").value;
+        var rows = document.getElementById("row").value;
+        var mines = document.getElementById("mine").value;
+        var numCells = cols * rows;
 
         if (isNaN(cols)||isNaN(rows)||isNaN(mines)||
                 cols<8||rows<8||cols>40||rows>30||mines<1||mines>(numCells)){
@@ -23,15 +22,15 @@ function startGame(){
         document.getElementById('status').innerHTML=('Click on the tiles to reveal them');
 
         remaining = mines;        // The number of mines remaining to be found.
-        tile=[];
-        board=[];
+        cells=[];
+        cellValues=[];
         revealed=0;                // The number of revealed tiles.
         for (i=0; i<=rows; i++){
                 for (j=0; j<=cols; j++){
-                        tile[i] = document.createElement('button');
-                        tile[i].addEventListener('mousedown',click);
-                        tile[i].id = i;
-                        document.body.appendChild(tile[i]);
+                        cells[i] = document.createElement('button');
+                        cells[i].addEventListener('mousedown',click);
+                        cells[i].id = i;
+                        document.body.appendChild(cells[i]);
                 }
         }
         /* for (i=0;i<rows*cols;i++) // Create the tiles.
@@ -49,14 +48,14 @@ function startGame(){
         placed=0;
         while (minesPlaced < mine){
                 i = Math.floor(Math.random()*col*row);
-                if (board[i]!='mine'){
-                        board[i] = 'mine';
+                if (cellValues[i]!='mine'){
+                        cellValues[i] = 'mine';
                         minesPlaced++;
                 }
         }
         
-        for (k=0; k<row; k++){
-                for (l=0; l<col; l++){
+        for (k=0; k<rows; k++){
+                for (l=0; l<cols; l++){
                         if (checkMap(l,k) != 'mine'){
                                 var sum;
                                 if (checkMap(l-1, k-1) == 'mine'){ //upper left corner
@@ -83,36 +82,35 @@ function startGame(){
                                 if (checkMap(l+1, k+1) == 'mine'){ //lower right corner
                                         sum++;
                                 }
-                                board[l+k*col] = sum;
+                                cellValues[l+k*col] = sum;
 
                         }
                 }
         }
 }
 
-function click(event)
-        {
+function click(event) {
         var source = event.target;
-                id=source.id;                        // The ID of the tile clicked by user.
+        id=source.id;                        // The ID of the tile clicked by user.
         if(event.which==3)        // On right click:
                 {
                 switch(picture(id))
                         {
-                        case 'x':tile[id].src='f.png';remaining--; break;         // If the tile is uncovered, set a flag.
-                        case 'f':tile[id].src='q.png';remaining++; break;         // If it's a flag, set a question mark.
-                        case 'q':tile[id].src='x.png';break;                        // If it's a question mark, set it to uncovered.
+                        case 'x':cells[id].src='f.png';remaining--; break;         // If the tile is uncovered, set a flag.
+                        case 'f':cells[id].src='q.png';remaining++; break;         // If it's a flag, set a question mark.
+                        case 'q':cells[id].src='x.png';break;                        // If it's a question mark, set it to uncovered.
                         }
                 event.preventDefault();
                 }
         document.getElementById('status').innerHTML="Mines remaining: "+remaining;                // Update the count of remaining mines.
         if(event.which==1&&picture(id)!='f')        // On left click if the tile is not a flag:
                 {
-                if(board[id]=='mine')        // if the tile is a mine:
+                if(cellValues[id]=='mine')        // if the tile is a mine:
                         {
                         for (i=0;i<rows*cols;i++)
                                 {
-                                if(board[i]=='mine') tile[i].src="m.png";        // show all the mines,
-                                if(board[i]!='mine'&&picture(i)=='f') tile[i].src="e.png";        // show a strike-through mine where flags were placed incorrectly.
+                                if(cellValues[i]=='mine') cells[i].src="m.png";        // show all the mines,
+                                if(cellValues[i]!='mine'&&picture(i)=='f') cells[i].src="e.png";        // show a strike-through mine where flags were placed incorrectly.
                                 }
                         document.getElementById('status').innerHTML='GAME OVER<br><br>Click here to restart';
                         }
@@ -125,12 +123,12 @@ function click(event)
         
 function reveal(index)        // Uncover the tile
         {
-        if(board[index]!='mine'&&picture(index)=="x")        // If it's covered and not a mine:
+        if(cellValues[index]!='mine'&&picture(index)=="x")        // If it's covered and not a mine:
                 revealed++;                 // If it was uncovered, increase the count of revealed tiles.
-                tile[index].src=board[index]+".png";        // Uncover the tile.
+                cells[index].src=cellValues[index]+".png";        // Uncover the tile.
                 var x=index%cols;        // Convert index into (x,y) coordinates.
                 var y=Math.floor(index/cols);
-                if(board[index]==0)        // If the value of the current tile is zero, check all the neighboring tiles:
+                if(cellValues[index]==0)        // If the value of the current tile is zero, check all the neighboring tiles:
                 {
                 if(x>0&&picture(index-1)=="x")        reveal(index-1);                                        // left
                 if(x<(cols-1)&&picture(+index+1)=="x") reveal(+index+1);                                // right
